3b4a106fc25c8223ac5308a664e3e1aed2197c30
Nate Cornelius <natecornelius@Nates-MBP-2.att.net>
2016-07-27 19:38:13 -0500

    add new views and complete dashboard

--- js/app.js
+++ js/app.js
@@ -6,8 +6,20 @@ app.config(['$routeProvider', function($routeProvider){
     //Application Routes
     $routeProvider.when('/', {
         templateUrl: 'partials/login.html'
+    }).when('/logout', {
+        templateUrl: 'partials/login.html'
     }).when('/dashboard', {
         templateUrl: 'partials/dashboard.html'
+    }).when('/income', {
+        templateUrl: 'partials/income.html'
+    }).when('/reports', {
+        templateUrl: 'partials/reports.html'
+    }).when('/scoreboard', {
+        templateUrl: 'partials/scoreboard.html'
+    }).when('/awards', {
+        templateUrl: 'partials/awards.html'
+    }).when('/account', {
+        templateUrl: 'partials/account.html'
     }).otherwise({
         redirectTo: '/'
     });
